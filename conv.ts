27847d5dd64ee255a9669d6d73fbb4a92bb18166
Gnlow <entrydark@gmail.com>
2020-05-24 13:36:38 +0900

    create csv

--- conv.ts
+++ conv.ts
@@ -1,6 +1,6 @@
-import boy from "./boy.js"
-import girl from "./girl.js"
-import { writeJson } from 'https://raw.githubusercontent.com/denoland/deno/v1.0.0-rc2/std/fs/write_json.ts'
+import boy from "./m.js"
+import girl from "./f.js"
+import { writeFileStr } from 'https://raw.githubusercontent.com/denoland/deno/v1.0.0-rc2/std/fs/write_file_str.ts'
 
-writeJson("./m.json", boy.map(({name, freq}: {name: string, freq: number}) => [name, freq]))
-writeJson("./f.json", girl.map(({name, freq}: {name: string, freq: number}) => [name, freq]))
+writeFileStr("./m.csv", (boy as [string,number][]).map(([name, freq]) => `${name},${freq}`).join("\n"))
+writeFileStr("./f.csv", (girl as [string,number][]).map(([name, freq]) => `${name},${freq}`).join("\n"))
