128bce6aeb3d74f572a0a01b69607ef17c9e8b57
Gnlow <entrydark@gmail.com>
2020-05-24 13:40:15 +0900

    Delete conv.ts

--- conv.ts
+++
@@ -1,6 +0,0 @@
-import boy from "./m.js"
-import girl from "./f.js"
-import { writeFileStr } from 'https://raw.githubusercontent.com/denoland/deno/v1.0.0-rc2/std/fs/write_file_str.ts'
-
-writeFileStr("./m.csv", (boy as [string,number][]).map(([name, freq]) => `${name},${freq}`).join("\n"))
-writeFileStr("./f.csv", (girl as [string,number][]).map(([name, freq]) => `${name},${freq}`).join("\n"))
